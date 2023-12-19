9581f9ab085bcb37742edcd44b67208926a0b7b8
yejin-km <124483450+yejin-km@users.noreply.github.com>
2023-12-18 23:07:53 -0500

    added axios and api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings: "quote goes here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: " ",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let userInput = document.querySelector("#user-input");
+
+  let apiKey = "60ba6250ab35e27b48473tdeo262aef3";
+  let context =
+    "You are great at inspirational quotes and love to inspire. Generate a short inspirational quote in basic html <p>your quote</p>. follow the user instructions.";
+  let prompt = `user instructions: generate an inspirational quote on ${userInput.value}`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(userInput.value);
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-form");
